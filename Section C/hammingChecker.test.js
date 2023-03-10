cb9641e1cad5aec02624aaa838fc450f3ac5c1a1
Simo Nkosi <wilzmar.kwd@gmail.com>
2023-03-10 10:47:49 +0200

    Add hammingChecker solution unit tests

---
+++ Section C/hammingChecker.test.js
@@ -0,0 +1,47 @@
+// Import the hammingChecker function from the solution file
+import hammingChecker from './hammingChecker';
+
+// Define a test suite using describe function
+describe("hammingChecker", () => {
+  // Define test cases using test function and expect function
+  test("uncorrupted block with length divisible by four", () => {
+    expect(hammingChecker("0111001011100100")).toBe("0010001011100100");
+  });
+
+  test("corrupted block with one flipped bit in position nine", () => {
+    expect(hammingChecker("0111001111100100")).toBe("1111001011100100");
+  });
+
+  test("corrupted block with one flipped bit in position zero", () => {
+    expect(hammingChecker("1110000011100100")).toBe("1011000011100100");
+  });
+
+  test("uncorrupted block with only one non-zero digit", () => {
+    expect(hammingChecker("0000000011100100")).toBe("0101000011100100");
+  });
+
+  test("uncorrupted block with length not divisible by four", () => {
+    expect(hammingChecker("10100101110010100")).toBe(
+      "11100100110010100"
+    );
+  });
+
+  test("corrupted block with one flipped bit in position sixteen", () => {
+    expect(hammingChecker("10100101110010101")).toBe(
+      "11100100110010111"
+    );
+  });
+
+  test('empty block', () => {
+
+    expect(hammingChecker('')).toBe('');
+
+  });
+
+  test('nil input', () => {
+
+    expect(() => hammingChecker(null)).toThrow();
+
+  });
+
+});
